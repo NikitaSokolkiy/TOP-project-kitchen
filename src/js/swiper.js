60bfdef55ecc591d232ff49019243a92bbbc1c50
Nikita Sokol <nikitasokolskiyy@gmail.com>
2024-05-07 16:02:13 +0300

    slider add

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,38 +1,12 @@
-import Swiper from "swiper";
-import 'swiper/css';
-
-import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
-// import Swiper and modules styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-
-// init Swiper:
-const swiper = new Swiper('.swiper', {
-    // configure Swiper to use modules
-    modules: [Navigation, Pagination],
-    ...
-});
-
 const swiper = new Swiper('.swiper', {
     // Optional parameters
-    direction: 'vertical',
+    direction: 'horizontal',
     loop: true,
-
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-    },
-
-    // Navigation arrows
+    slidesPerView: 3,
+    spaceBetween: 30,
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
-
-    // And if we need scrollbar
-    scrollbar: {
-        el: '.swiper-scrollbar',
-    },
 });
+
