81b1408318b1c25f2ffd9d9fd57ebb30a7a1a55d
Nikita Sokol <nikitasokolskiyy@gmail.com>
2024-05-15 13:51:21 +0300

    ...

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -2,11 +2,26 @@ const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
-    slidesPerView: 3,
-    spaceBetween: 50,
+    breakpoints:{
+        1100:{
+          slidesPerView: 3
+        },
+        760: {
+            slidesPerView: 2,
+            // centeredSlides: true,
+            // spaceBetween: 30,
+            // grabCursor: true,
+        }
+    },
     navigation: {
         nextEl: '.button-swiper__right',
         prevEl: '.button-swiper__left',
     },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+
 });
 
